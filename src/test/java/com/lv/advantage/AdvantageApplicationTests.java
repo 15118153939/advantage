a11fff59ee8ec3e13ca29704591607e2a36f23ed
吕明亮 <2850208173@qq.com>
2019-11-19 11:29:48 +0800

    Initial commit

---
+++ src/test/java/com/lv/advantage/AdvantageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lv.advantage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvantageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
